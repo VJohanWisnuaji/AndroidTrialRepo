526bc27195dab5d71e51c01089b66e460a51df53
Nanda Adi <nandaadis186@gmail.com>
2020-06-24 12:53:52 +0700

    Commat Commit lagi

--- app/src/main/java/com/studytrial/mytrialapp1/MainActivity.kt
+++ app/src/main/java/com/studytrial/mytrialapp1/MainActivity.kt
@@ -8,7 +8,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //stash
+        //ini buatan nanda
     }
 
     fun newFun (){
